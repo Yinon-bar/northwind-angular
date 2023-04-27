8ba02cef92865ac84ca230b81e8e5435a97a20cf
Yinon Bar <yinonbar1988@gmail.com>
2023-04-27 20:07:00 +0300

    style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,19 @@ import { AboutComponent } from './components/about-area/about/about.component';
 import { HomeComponent } from './components/layout-area/home/home.component';
 import { PageNotFoundComponent } from './components/layout-area/page-not-found/page-not-found.component';
 import { ProductsListComponent } from './components/products-area/products-list/products-list.component';
+import { AddProductComponent } from './components/products-area/add-product/add-product.component';
 
 const routes: Routes = [
-    {path: 'home', component: HomeComponent },
-    {path: 'products', component: ProductsListComponent },
-    {path: 'about', component: AboutComponent },
-    {path: '', redirectTo: '/home', pathMatch: 'full'},
-    {path: '**', component: PageNotFoundComponent}
-
+  { path: 'home', component: HomeComponent },
+  { path: 'products', component: ProductsListComponent },
+  { path: 'products/new', component: AddProductComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
